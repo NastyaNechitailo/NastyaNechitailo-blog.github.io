dfb5f6dd4b8d47ee91a20af34711f07431ce3e32
Nastya <nastyarivz23@gmail.com>
2020-08-25 11:22:03 +0300

    added mobile menu, added form

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// mobile menu
 let mobileMenu = document.querySelector(".nav-mobile-menu");
 let mainMenu = document.querySelector(".navigation");
 
@@ -7,5 +8,41 @@ mobileMenu.addEventListener("click", function() {
         mainMenu.classList.add("active-menu")
     } else {
         mainMenu.classList.remove("active-menu")
+    }
+})
+
+// form to send message
+let showMessageButton = document.querySelector(".form-button");
+    let form = document.querySelector(".form");
+    
+    showMessageButton.addEventListener("click", function() {
+        form.classList.toggle("show")
+    })
+
+// scroll form
+    let modal = document.querySelector(".modal");
+    let buttonClose = document.querySelector(".close");
+    let scrollHeight = document.body.scrollHeight / 2;
+    console.log(scrollHeight)
+
+    window.addEventListener("scroll", function() {
+    let scrollDistance = window.scrollY;
+    if(scrollDistance == scrollHeight) {
+        modal.classList.add("show")
+        modal.classList.remove("hide")
+        console.log("skjnva")
+    } 
+})
+
+function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+}
+
+buttonClose.addEventListener("click", closeModal)
+
+modal.addEventListener("click", function(e) {
+    if(e.target === modal) {
+        closeModal()
     }
 })
